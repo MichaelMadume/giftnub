eeceded2678c4ab6f398c2464fdf9af19fcc1305
Immaculate <iegwu@pajuno.com>
2024-01-11 12:26:22 +0100

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
+const { join } = require('path');
+
+module.exports = {
+  content: [
+    join(__dirname, './apps/giftnub-web/src/**/!(*.stories|*.spec).{ts,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
