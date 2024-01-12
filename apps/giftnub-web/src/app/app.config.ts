e591dc69476b9b9e12adf4fe4279fbbb20145b39
Immaculate <iegwu@pajuno.com>
2024-01-12 10:42:56 +0100

    add nebular

--- apps/giftnub-web/src/app/app.config.ts
+++ apps/giftnub-web/src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
+import { NbLayoutDirection, NbThemeModule } from '@nebular/theme';
+import { DEFAULT_THEME } from '../styles/theme.default';
+import { DARK_THEME } from '../styles/theme.dark';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(appRoutes), provideServiceWorker('ngsw-worker.js', {
         enabled: !isDevMode(),
         registrationStrategy: 'registerWhenStable:30000'
-    })],
+    }),    importProvidersFrom(
+      NbThemeModule.forRoot(
+        {
+          name: window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'dark'
+            : 'default',
+        },
+        [DEFAULT_THEME, DARK_THEME],
+        undefined,
+        NbLayoutDirection.LTR
+      )
+    ),],
 };
---
+++ apps/giftnub-web/src/styles/theme.dark.ts
@@ -0,0 +1,9 @@
+import { NbJSThemeOptions, DARK_THEME as baseTheme } from '@nebular/theme';
+
+baseTheme.variables;
+
+export const DARK_THEME = {
+  name: 'dark',
+  base: 'dark',
+  variables: {},
+} as NbJSThemeOptions;
