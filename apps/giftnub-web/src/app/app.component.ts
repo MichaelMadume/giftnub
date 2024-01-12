18c0528ab5117abe960f677e50a0cb1ce13ef1cc
Immaculate <iegwu@pajuno.com>
2024-01-12 13:29:55 +0100

    Update nebular layout

--- apps/giftnub-web/src/app/app.component.ts
+++ apps/giftnub-web/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { NbLayoutModule } from '@nebular/theme';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, NbLayoutModule],
   selector: 'giftnub-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
