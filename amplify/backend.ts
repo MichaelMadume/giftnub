0b50dc69b53c38dd2b0c4bbe97f0941d9a20870c
MichaelMadume <m.c.madume@gmail.com>
2025-02-18 16:35:54 +0100

    Revert "push dummy backend"
    This reverts commit 5b4552400668e767bb080ac8a1c5c8b1b3e631bf.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,8 @@
-import { defineBackend } from "@aws-amplify/backend";
-import { myFirstFunction } from "./my-first-function/resource";
+import { defineBackend } from '@aws-amplify/backend';
+import { auth } from './auth/resource';
+import { data } from './data/resource';
 
-export const backend = defineBackend({
-  myFirstFunction
+defineBackend({
+  auth,
+  data,
 });
